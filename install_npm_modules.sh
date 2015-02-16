4d0dfd89aa85258796b736f03209d05c2844075d
Bruno Kindt <bruno.kindt@gmail.com>
2015-02-16 16:13:33 +0100

    add global npm module installation script

---
+++ install_npm_modules.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env sh
+
+# express project generator
+npm install -g express-generator
+# CoffeeScript
+npm install -g coffee-script
+# watch files in pwd and restart nodejs server
+npm install -g nodemon
+# grunt
+npm install -g grunt-cli
+# jshint
+npm install -g jshint
+# frontend package manager
+npm install -g bower
+# responsive screenshots
+npm install -g pageres
+# phantomjs
+npm install -g phantomjs
+# mean stack
+npm install -g mean-cli
+# debug nodejs from a browser
+npm install -g node-inspector
+npm install -g yo
+npm install -g generator-meanjs
+npm install -g gerenator-h5bp
+npm install -g generator-angular-fullstack
+npm install -g generator-gulp-webapp
+npm install -g generator-mocha
+npm install -g generator-angular-require
+npm install -g generator-karma-require
+# swig task runner
+npm install -g swig
+# mocha test framework
+npm install -g mocha
+# gulp
+npm install -g gulp
+# jade
+npm install -g jade
+# angularJS selinium test runner
+npm install -g protractor
+# browserify
+npm install -g browserify
