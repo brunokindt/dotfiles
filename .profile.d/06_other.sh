34d8a88d2d14f04df2b8eafeef6ee73224636f13
bruno.kindt <bruno.kindt@poembyrabb.it>
2015-07-19 11:55:31 +0200

    Add nvm command

--- .profile.d/06_other.sh
+++ .profile.d/06_other.sh
@@ -1,3 +1,8 @@
 # touchpad natural scrolling
 synclient VertScrollDelta=-111
 synclient HorizScrollDelta=-111
+
+# NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  
+
