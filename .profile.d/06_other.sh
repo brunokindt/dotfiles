d0ea66148695e79506ac806ff15a34cc16d62b05
Bruno Kindt <bruno.kindt@gmail.com>
2015-07-25 14:01:29 +0200

    Update profile.d
    - Add zeitgeist daemon
    - Set X root window background color
    - Set Xdefaults

--- .profile.d/06_other.sh
+++ .profile.d/06_other.sh
@@ -4,5 +4,12 @@ synclient HorizScrollDelta=-111
 
 # NVM
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+# ZEITGEIST
+if hash zeitgeist-daemon 2> /dev/null; then
+    /usr/bin/zeitgeist-daemon --replace &
+fi
+
+/usr/bin/xsetroot -solid "#020202"
+xrdb -merge ~/.Xdefaults
