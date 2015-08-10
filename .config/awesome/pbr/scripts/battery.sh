6421b534c8a5caebdfee9583593738765746a9c3
brunokindt <bruno.kindt@poembyrabb.it>
2015-08-10 15:08:25 +0200

    Remove awesomeWM configuration
    - move awesomeWM configuration to git@github.com:brunokindt/awesomeWM-dotfiles.git

--- .config/awesome/pbr/scripts/battery.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd "/sys/class/power_supply/BAT0" &&
-status=$(cat status)
-current=$(cat energy_now)
-full=$(cat energy_full)
-#percent=`echo "scale=1; $current / $full * 100" | bc`
-percent=`echo "scale=2; $(cat energy_now) / $(cat energy_full) * 100" | bc`
-echo "${percent}% ${status}"
