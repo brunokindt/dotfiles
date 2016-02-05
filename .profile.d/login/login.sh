855101c58742ae90f31575babd83b67476fbf042
brunokindt <bruno.kindt@poembyrabb.it>
2016-02-05 20:19:23 +0100

    Fix shell login script

--- .profile.d/login/login.sh
+++ .profile.d/login/login.sh
@@ -1,15 +1,20 @@
-# touchpad natural scrolling
-synclient VertScrollDelta=-111
-synclient HorizScrollDelta=-111
+if [ ! -z $DISPLAY ]; then
 
-# NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  # touchpad natural scrolling
+    synclient VertScrollDelta=-111
+    synclient HorizScrollDelta=-111
+  
+  # NVM
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  
+  # ZEITGEIST
+    if hash zeitgeist-daemon 2> /dev/null; then
+        /usr/bin/zeitgeist-daemon --replace &
+    fi
+  
+    /usr/bin/xsetroot -solid "#020202"
+  xrdb -merge ~/.Xdefaults
 
-# ZEITGEIST
-if hash zeitgeist-daemon 2> /dev/null; then
-    /usr/bin/zeitgeist-daemon --replace &
-fi
+fi 
 
-/usr/bin/xsetroot -solid "#020202"
-xrdb -merge ~/.Xdefaults
